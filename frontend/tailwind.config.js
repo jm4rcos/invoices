aebf3ca45dbc5a61209bf00badc853318726d21f
jm4rcos <joaomarcosmacimiro@gmail.com>
2024-10-17 03:23:33 -0300

    add: tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var--background)",
+        primary: "hsl(var--primary)",
+        secondary: "hsl(var--secondary)",
+        muted: "hsl(var--muted)",
+        accent: "hsl(var--accent)",
+        text: "hsl(var--text)",
+        title: "hsl(var--title)",
+      },
+    },
   },
   plugins: [],
 };
